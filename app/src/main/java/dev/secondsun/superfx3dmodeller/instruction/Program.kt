e86eb37f11d435234a47f7c6ff5c29655a96ace0
Summers Pittman <secondsun@gmail.com>
2022-10-29 11:32:03 -0400

    wip

--- app/src/main/java/dev/secondsun/superfx3dmodeller/instruction/Program.kt
+++ app/src/main/java/dev/secondsun/superfx3dmodeller/instruction/Program.kt
@@ -2,6 +2,8 @@ package dev.secondsun.superfx3dmodeller.instruction
 
 import dev.secondsun.superfx3dmodeller.cpu.SuperFX
 
+
+
 class Program(val bank: Byte = 0,val  address: Short = 0,val  instructions: SuperFX.() -> Unit) {
 
     lateinit var state:SuperFX;
